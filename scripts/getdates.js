3c6cca526d5ec04fa957c5a8563e359272b73fd4
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-03-08 16:25:50 -0500

    new

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,30 @@
-const currentyear = document.querySelector("#year");
+const currentyear = document.querySelector("#currentyear");
+
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+
+const nowtime = new Date();
+
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+
+
+
+/*lastModified.innerHTML = `${today.getTime()}`;*/
+
+
+lastModified.innerHTML = `${new Intl.DateTimeFormat(
+	"en-UK",
+	{
+		timeStyle: "medium"
+	}
+).format(today),
+
+new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(nowtime)}`;
