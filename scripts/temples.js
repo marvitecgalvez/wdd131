aea3cb8ce7edd95aa896d0be74f466d7d7eab39f
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-03-19 10:05:22 -0500

    new

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,19 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+const currentyear = document.querySelector("#currentyear");
+
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+
+const nowtime = new Date();
+
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+
+
+
+lastModified.innerHTML = document.lastModified;
