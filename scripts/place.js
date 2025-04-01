2cb0f001b0cd5dd59adc0b24fa9ad946c16c8279
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-04-01 15:47:35 -0500

    new

--- scripts/place.js
+++ scripts/place.js
@@ -16,7 +16,7 @@ lastModified.innerHTML = document.lastModified;
 
 
 //Windchill Calcuator
-
+/*
 const windDisplay = document.querySelector("#windchill");
 let temperature = document.getElementById("temperature")
 let windSpeed = document.getElementById("windspeed")
@@ -32,7 +32,7 @@ function calculateWindChill(temperature, windSpeed) {
   let windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * T * temperature * Math.pow(windSpeed, 0.16);
     /*35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);*/
 
-  return windChill.toFixed(2);
+  /*return windChill.toFixed(2);
 }
 
 windChill = calculateWindChill(temperature, windSpeed);
@@ -41,4 +41,4 @@ if (temperature <= 10 && windSpeed > 4.8) {
   windDisplay.innerHTML = `${windChill} °C`;
 } else {
   windDisplay.innerHTML = `N/A`;
-}
+}*/
